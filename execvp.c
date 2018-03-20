cca17afd624af05082d58a655e17d446cc1ee62c
prahate <patyarahate@gmail.com>
2018-03-20 19:48:55 +0530

    Program illustrating use of execvp

---
+++ execvp.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main () {
+	printf("Hello PID: %d\n", (int) getpid());
+	
+	int rc = fork();	//creating new process using fork
+
+	if ( rc < 0 ) {
+		printf("Failed to create process using fork\n");
+		exit(1);
+	}
+	else if ( rc == 0 ) {	//child process
+		printf("Hello I am child PID : %d\n", (int) getpid());
+		char *args[] = {"./hello", NULL};
+		execvp(args[0], args);
+		printf("This line wont be printed\n");
+	}
+	else {			//Parent process
+		int wc = wait(NULL);
+		printf("Hello I am Parent PID: %d\n", (int) getpid());
+	}
+
+return 0;
+}
