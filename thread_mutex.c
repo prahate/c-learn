8f46ced3f7c3ba8011cec02777f7134aaed887e2
Prathamesh Rahate <patyarahate@gmail.com>
2019-07-28 21:42:59 +0530

    Adding mutex to solve issue with concurrency

--- thread_mutex.c
+++ thread_mutex.c
@@ -4,16 +4,31 @@
 #include <pthread.h>
 
 static int glob=0;
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;		//Statically initialized mutex
+
+
 
 static void *threadFunc(void * arg) {
 
 	int loops = *((int *)arg);
-	int loc, j;
+	int loc, j, s;
 
 	for (j=0; j < loops; j++) {
+		s = pthread_mutex_lock(&mtx);
+		if (s != 0) {
+			perror("pthread_mutex_lock");
+			exit(-1);
+		}
+
 		loc = glob;
 		loc++;
 		glob = loc;
+
+		s = pthread_mutex_unlock(&mtx);
+		if (s != 0) {
+			perror("pthread_mutex_unlock");
+			exit(-1);
+		}
 	}
 
 	return NULL;
