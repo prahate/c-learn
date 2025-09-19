98d5ecce5c5cb604e5652d40697317e6a4c30e94
Prathamesh Rahate <patyarahate@gmail.com>
2025-09-19 14:23:09 +0530

    Added insert and tree traversals t print the tree

--- dsa/bst.c
+++ dsa/bst.c
@@ -9,7 +9,7 @@ typedef struct node {
     struct node *right;
 } node_t;
 
-static node_t *root=NULL; // pointer to root node of tree
+static node_t root; // pointer to root node of tree
 
 node_t * new_node(int data)
 {
@@ -34,12 +34,78 @@ void delete_node(node_t *n)
 void insert(int data, node_t *rootnode)
 {
     // case 1: is tree is empty or has no nodes
-    if (root == NULL)
-        root = new_node(data);
+    if (rootnode == NULL)
+        rootnode = new_node(data);
     else if (data < rootnode->data) {
         // case 2: insert to left subtree
-    } 
+        if ( rootnode->left == NULL)
+            rootnode->left = new_node(data);
+        else
+            insert(data, rootnode->left);
+    }
     else if (data > rootnode->data) {
         // case 3: insert to right subtree
+        if (rootnode->right == NULL)
+            rootnode->right = new_node(data);
+        else
+            insert(data, rootnode->right);
     }
+}
+
+void inorder(node_t *rootnode)
+{
+    if (rootnode == NULL)
+        return;
+    inorder(rootnode->left);
+    if (rootnode->data)
+        printf("%d ", rootnode->data);
+    inorder(rootnode->right);
+}
+
+void preorder(node_t *rootnode)
+{
+    if (rootnode == NULL)
+        return;
+    if (rootnode->data)
+        printf("%d ", rootnode->data);
+    preorder(rootnode->left);
+    preorder(rootnode->right);
+}
+
+void postorder(node_t *rootnode)
+{
+    if(rootnode == NULL)
+        return;
+
+    postorder(rootnode->left);
+    postorder(rootnode->right);
+    if (rootnode->data)
+        printf("%d ", rootnode->data);
+}
+
+int main()
+{
+    insert(11, &root);
+    insert(6, &root);
+    insert(15, &root);
+    insert(3, &root);
+    insert(8, &root);
+    insert(13, &root);
+    insert(17, &root);
+    insert(1, &root);
+    insert(5, &root);
+    insert(12, &root);
+    insert(14, &root);
+    insert(19, &root);
+
+    printf("Inorder : ");
+    inorder(&root);
+    printf("\n");
+    printf("Preorder : ");
+    preorder(&root);
+    printf("\n");
+    printf("Postorder : ");
+    postorder(&root);
+    printf("\n");
+    return 0;
 }
