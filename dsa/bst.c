ca46b209bcc008cef231fb2406bb630b23125975
Prathamesh Rahate <patyarahate@gmail.com>
2025-09-21 00:24:26 +0530

    Fixed issue root node tree creation

--- dsa/bst.c
+++ dsa/bst.c
@@ -9,7 +9,7 @@ typedef struct node {
     struct node *right;
 } node_t;
 
-static node_t root; // pointer to root node of tree
+static node_t *root; // pointer to root node of tree
 
 node_t * new_node(int data)
 {
@@ -31,24 +31,24 @@ void delete_node(node_t *n)
 }
 
 
-void insert(int data, node_t *rootnode)
+node_t* insert(node_t *rootnode, int data)
 {
     // case 1: is tree is empty or has no nodes
     if (rootnode == NULL)
-        rootnode = new_node(data);
+        return new_node(data);
     else if (data < rootnode->data) {
         // case 2: insert to left subtree
         if ( rootnode->left == NULL)
             rootnode->left = new_node(data);
         else
-            insert(data, rootnode->left);
+            insert(rootnode->left, data);
     }
     else if (data > rootnode->data) {
         // case 3: insert to right subtree
         if (rootnode->right == NULL)
             rootnode->right = new_node(data);
         else
-            insert(data, rootnode->right);
+            insert(rootnode->right, data);
     }
 }
 
@@ -85,27 +85,27 @@ void postorder(node_t *rootnode)
 
 int main()
 {
-    insert(11, &root);
-    insert(6, &root);
-    insert(15, &root);
-    insert(3, &root);
-    insert(8, &root);
-    insert(13, &root);
-    insert(17, &root);
-    insert(1, &root);
-    insert(5, &root);
-    insert(12, &root);
-    insert(14, &root);
-    insert(19, &root);
+    root = insert(NULL, 11);
+    insert(root, 6);
+    insert(root, 15);
+    insert(root, 3);
+    insert(root, 8);
+    insert(root, 13);
+    insert(root, 17);
+    insert(root, 1);
+    insert(root, 5);
+    insert(root, 12);
+    insert(root, 14);
+    insert(root, 19);
 
     printf("Inorder : ");
-    inorder(&root);
+    inorder(root);
     printf("\n");
     printf("Preorder : ");
-    preorder(&root);
+    preorder(root);
     printf("\n");
     printf("Postorder : ");
-    postorder(&root);
+    postorder(root);
     printf("\n");
     return 0;
 }
